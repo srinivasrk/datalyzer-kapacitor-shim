1c2d6b69f069855f2cacec04af9fbf8c4c9cf065
srinivasrk <srinivasrk92@gmail.com>
2018-07-25 14:29:30 -0400

    add console logging

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ app.all('/', function(req, res){
   res.status(200).send('Datalyzer-Kapacitor-Shim')
 })
 app.all('/getGaps', function(req, res) {
-  console.log(req.headers)
+  console.log(new Date() + " Request for GET: /getGaps")
   //here get the header params for the request which has generator, meaasurement and datarange
   let {generator} = req.headers
   let {measurement} = req.headers
   let {daterange} = req.headers
 
-  console.log(daterange + "" + measurement + "" + generator)
   reqData.getFromKapacitor(generator, measurement, daterange).then((response) => {
-    console.log(response)
     res.send(response)
     res.end()
   })
